538afa0db4911ca75bb7c2e949f2baa8d1c47da0
vot24100 <mladen.m.savov@gmail.com>
2015-08-30 15:07:55 +0300

    Added a simple unit test file "login_test.js" and a HTML Reporter file "Index.html"

---
+++ Theme/tests/login_test.js
@@ -0,0 +1,13 @@
+
+var expect = chai.expect;
+
+describe("Login", function() {
+    describe("#tryToLogUserIn", function() {
+        it("expect the function to exist", function() {            
+            expect(tryToLogUserIn).to.be.a('function');
+        });
+
+    });
+
+    
+});
