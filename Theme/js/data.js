1f3e2ddeadd3421d7fa02a53facabfa00cc2d358
Geogri Manov <georgimanov@gmail.com>
2015-08-31 00:35:29 +0300

    Work on competitions

--- Theme/js/data.js
+++ Theme/js/data.js
@@ -77,8 +77,30 @@ export default dataPersister => {
                         query.get(competitionId, {
                             success: function (competition) {
                                 console.log(competition);
-                                $('#competition-view').append('<h3>' + competition.get('title') + '</h3>');
-                                $('#competition-view').append('<h4>' + 'Zero competitors here' + '</h4>');
+                                $('#competition-view').append(
+                                '<div class="content-section-b">'+
+                                '<div class="container">'+
+                                '<div class="row">'+
+                                '<div class="col-lg-5 col-lg-offset-1 col-sm-push-6  col-sm-6">'+
+                                '<img class="img-responsive" src="img/dog.png" alt="">'+
+                                '</div>'+
+                                '<div class="col-lg-5 col-sm-pull-6  col-sm-6">'+
+                                '<div class="clearfix"></div>'+
+                                '<h2 class="section-heading">' +  competition.get('title') +
+                                '</h2>'+
+                                '<hr class="section-heading-spacer">'+
+                                '<div class="clearfix"></div>'+
+                                '<p class="lead">' +
+                                competition.get('description') +
+                                '</p>'+
+                                '<p><span class="text-primary">'+'Start: ' + '</span>' + helper().formatDate(competition.get('start')) + '</p>'+
+                                '<p><span class="text-primary">'+'End: ' + '</span>' + helper().formatDate(competition.get('end')) +'</p>' +
+                                '<a href="#" class="btn btn-success btn-lg competition-join" data-competition-join-id="'+ competition['id'] + '">Be the first to JOIN</a>'+
+                                '</div>'+
+                                '</div>'+
+                                '</div>'+
+                                '</div>'
+                                );
                             },
                             error: function (error) {
                                 console.log("Error: " + error.code + " " + error.message);
@@ -88,14 +110,50 @@ export default dataPersister => {
 
                     var cmp = competition[0]['attributes']['competition'];
                     console.log(competition);
-                    $('#competition-view').append('<h3>' + cmp.get('title') + '</h3>');
+                    $('#competition-view').append(
+                        '<div class="content-section-b">'+
+                        '<div class="container">'+
+                        '<div class="row">'+
+                        '<div class="col-lg-5 col-lg-offset-1 col-sm-push-6  col-sm-6">'+
+                        '<img class="img-responsive" src="img/dog.png" alt="">'+
+                        '</div>'+
+                        '<div class="col-lg-5 col-sm-pull-6  col-sm-6">'+
+                        '<div class="clearfix"></div>'+
+                        '<h2 class="section-heading">' +  cmp.get('title') +
+                        '</h2>'+
+                        '<hr class="section-heading-spacer">'+
+                        '<div class="clearfix"></div>'+
+                        '<p class="lead">' +
+                        cmp.get('description') +
+                        '</p>'+
+                        '<p><span class="text-primary">'+'Start: ' + '</span>' + helper().formatDate(cmp.get('start')) + '</p>'+
+                        '<p><span class="text-primary">'+'End: ' + '</span>' + helper().formatDate(cmp.get('end')) +'</p>' +
+                        '<a href="#" class="btn btn-success btn-lg competition-join" data-competition-join-id="'+ cmp['id'] + '">JOIN</a>'+
+                        '</div>'+
+                        '</div>'+
+                        '</div>'+
+                        '</div>'
+                    );
+
+                        $('#competition-view').append('<h3>Competitors</h3>'+
+                            '<hr class="section-heading-spacer">'+
+                        '<table class="table table-striped table-hover " id="competition-table">'+
+                        '<thead>'+
+                        '<tr>'+
+                        '<th>#</th>'+
+                        '<th>Username</th>'+
+                        '<th>Email</th>'+
+                        '</tr>'+
+                        '</thead>'+
+                        '<tbody>');
+
                         var counter = 1,
                             i;
                     for(i = 0; i< competition.length; i++){
                         var competitor = competition[i]['attributes']['user'];
-                        $('#competition-view').append('<p class="text-success">[' + (counter++) + '] ' + competitor.get('username') + ' -> ' + competitor.get('email') + '</p>');
-
+                        $('#competition-table').append('<tr><td>' + (counter++) + '</td><td>' + competitor.get('username') + '</td><td><a href="mailto:'+ competitor.get('email') +'?Subject=Hello" target="_top">' + competitor.get('email') + '</a></td></tr>');
                     }
+                        $('#competition-view').append('</tbody></table>');
                     }
                 },
                 error: function(error){
