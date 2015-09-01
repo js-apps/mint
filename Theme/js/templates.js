73d7c966e5f6904439a895a60540f1574d128732
shunobaka <vesselin_minev@abv.bg>
2015-09-01 20:19:02 +0300

    Created templates.js with functionality to retrieve and compile templates

---
+++ Theme/js/templates.js
@@ -0,0 +1,20 @@
+var templates = (function() {
+    function get(templateName) {
+        var url = 'templates/' + templateName + '.handlebars';
+
+        var promise = new Promise(function(resolve, reject) {
+            $.get(url, function(templateHtml) {
+            	var template = Handlebars.compile(templateHtml);
+            	resolve(template);
+            }).fail(function() {
+            	reject();
+            });
+        });
+
+        return promise;
+    }
+
+    return {
+        get: get
+    };
+})();
