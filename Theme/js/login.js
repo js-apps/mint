d0342e702c886c25f84f3dfccc8eb8a477a47d5a
shunobaka <vesselin_minev@abv.bg>
2015-09-01 12:36:33 +0300

    Fixed and improved login.js and register.js

--- Theme/js/login.js
+++ Theme/js/login.js
@@ -1,19 +1,22 @@
-import user from 'js/user-functionalities';
+import userButtons from 'js/user-buttons';
 
-function tryToLogUserIn(username, password) {
-	Parse.User.logIn(username, password, {
-		success: function() {
-		    user().setUserButtons();
-		},
-		error: function(triedUser, error) {
-		    $('#login-error-label')
-		    	.html('The username or password is incorrect.');
-		}
-	});
-}
+$(function() {
+    function tryToLogUserIn(username, password) {
+        Parse.User.logIn(username, password, {
+            success: function() {
+                userButtons().set();
+                window.location.href = "/#/home";
+            },
+            error: function(triedUser, error) {
+                $('#login-error-label')
+                    .html('The username or password is incorrect.');
+            }
+        });
+    }
 
-$('#main-container').on('click', '#login-submit', function() {
-	var username = $('#username-login').val();
-	var password = $('#password-login').val();
-	tryToLogUserIn(username, password);
+    $('#main-container').on('click', '#login-submit', function() {
+        var username = $('#username-login').val();
+        var password = $('#password-login').val();
+        tryToLogUserIn(username, password);
+    });
 });
--- Theme/js/register.js
+++ Theme/js/register.js
@@ -1,108 +1,111 @@
 import validator from 'js/validator';
-import user from 'js/user-functionalities';
+import userButtons from 'js/user-buttons';
 
-function registerUser(username, password, email) {
-	var newUser = new Parse.User();
-	newUser.set("username", username);
-	newUser.set("password", password);
-	newUser.set("email", email);
+$(function() {
+    function registerUser(username, password, email) {
+        var newUser = new Parse.User();
+        newUser.set("username", username);
+        newUser.set("password", password);
+        newUser.set("email", email);
 
-	newUser.signUp(null, {
-	  	success: function() {
-	    	user().setUserButtons();
-	  	},
-	  	error: function(newUser, error) {
-	    	$('#register-error-label')
-	    		.html('Email or username is already in use.');
-	  	}
-	});
-}
+        newUser.signUp(null, {
+            success: function() {
+                userButtons().set();
+                window.location.href = "/#/home";
+            },
+            error: function(newUser, error) {
+                $('#register-error-label')
+                    .html('Email or username is already in use.');
+            }
+        });
+    }
 
-$('#main-container').on('input', '#username-reg', function(ev) {
-	var target = ev.target;
-	var usernameValidationResult = validator().validateUsername(target.value);
+    $('#main-container').on('input', '#username-reg', function(ev) {
+        var target = ev.target;
+        var usernameValidationResult = validator().validateUsername(target.value);
 
-	if (usernameValidationResult.isValid) {
-		$(target).parent().removeClass('has-error').addClass('has-success');
-		$(target).next().html(usernameValidationResult.message);
-	} else {
-		$(target).parent().removeClass('has-success').addClass('has-error');
-		$(target).next().html(usernameValidationResult.message);
-	}
-});
+        if (usernameValidationResult.isValid) {
+            $(target).parent().removeClass('has-error').addClass('has-success');
+            $(target).next().html(usernameValidationResult.message);
+        } else {
+            $(target).parent().removeClass('has-success').addClass('has-error');
+            $(target).next().html(usernameValidationResult.message);
+        }
+    });
 
-$('#main-container').on('input', '#password-reg', function(ev) {
-	var target = ev.target;
-	var passwordValidationResult = validator().validatePassword(target.value);
+    $('#main-container').on('input', '#password-reg', function(ev) {
+        var target = ev.target;
+        var passwordValidationResult = validator().validatePassword(target.value);
 
-	if (passwordValidationResult.isValid) {
-		$(target).parent().removeClass('has-error').addClass('has-success');
-		$(target).next().html(passwordValidationResult.message);
-	} else {
-		$(target).parent().removeClass('has-success').addClass('has-error');
-		$(target).next().html(passwordValidationResult.message);
-	}
+        if (passwordValidationResult.isValid) {
+            $(target).parent().removeClass('has-error').addClass('has-success');
+            $(target).next().html(passwordValidationResult.message);
+        } else {
+            $(target).parent().removeClass('has-success').addClass('has-error');
+            $(target).next().html(passwordValidationResult.message);
+        }
 
-	if ($('#confirm-password-reg')[0].value.length !== 0) {
-		$('#confirm-password-reg').trigger('input');
-	}
-});
+        if ($('#confirm-password-reg')[0].value.length !== 0) {
+            $('#confirm-password-reg').trigger('input');
+        }
+    });
 
-$('#main-container').on('input', '#confirm-password-reg', function(ev) {
-	var target = ev.target;
-	var passwordMatchValidationResult = validator().validateMatchingPasswords($('#password-reg').val(), target.value);
+    $('#main-container').on('input', '#confirm-password-reg', function(ev) {
+        var target = ev.target;
+        var passwordMatchValidationResult = validator().validateMatchingPasswords($('#password-reg').val(), target.value);
 
-	if (passwordMatchValidationResult.isValid) {
-		$(target).parent().removeClass('has-error').addClass('has-success');
-		$(target).next().html(passwordMatchValidationResult.message);
-	} else {
-		$(target).parent().removeClass('has-success').addClass('has-error');
-		$(target).next().html(passwordMatchValidationResult.message);
-	}
-});
+        if (passwordMatchValidationResult.isValid) {
+            $(target).parent().removeClass('has-error').addClass('has-success');
+            $(target).next().html(passwordMatchValidationResult.message);
+        } else {
+            $(target).parent().removeClass('has-success').addClass('has-error');
+            $(target).next().html(passwordMatchValidationResult.message);
+        }
+    });
 
-$('#main-container').on('input', '#email-reg', function(ev) {
-	var target = ev.target;
-	var emailValidationResult = validator().validateEmail(target.value);
+    $('#main-container').on('input', '#email-reg', function(ev) {
+        var target = ev.target;
+        var emailValidationResult = validator().validateEmail(target.value);
 
-	if (emailValidationResult.isValid) {
-		$(target).parent().removeClass('has-error').addClass('has-success');
-		$(target).next().html(emailValidationResult.message);
-	} else {
-		$(target).parent().removeClass('has-success').addClass('has-error');
-		$(target).next().html(emailValidationResult.message);
-	}
-});
+        if (emailValidationResult.isValid) {
+            $(target).parent().removeClass('has-error').addClass('has-success');
+            $(target).next().html(emailValidationResult.message);
+        } else {
+            $(target).parent().removeClass('has-success').addClass('has-error');
+            $(target).next().html(emailValidationResult.message);
+        }
+    });
 
-$('#main-container').on('click', '#register-submit', function() {
-	var username = $('#username-reg').val();
-	var password = $('#password-reg').val();
-	var passwordConfirm = $('#confirm-password-reg').val();
-	var email = $('#email-reg').val();
+    $('#main-container').on('click', '#register-submit', function() {
+        var username = $('#username-reg').val();
+        var password = $('#password-reg').val();
+        var passwordConfirm = $('#confirm-password-reg').val();
+        var email = $('#email-reg').val();
 
-	var usernameValidationResult = validator().validateUsername(username);
-	var passwordValidationResult = validator().validatePassword(password);
-	var passwordMatchValidationResult = validator().validateMatchingPasswords(password, passwordConfirm);
-	var emailValidationResult = validator().validateEmail(email);
+        var usernameValidationResult = validator().validateUsername(username);
+        var passwordValidationResult = validator().validatePassword(password);
+        var passwordMatchValidationResult = validator().validateMatchingPasswords(password, passwordConfirm);
+        var emailValidationResult = validator().validateEmail(email);
 
-	var usernameIsValid = usernameValidationResult.isValid;
-	var passwordIsValid = passwordValidationResult.isValid;
-	var passwordsMatch = passwordMatchValidationResult.isValid;
-	var emailIsValid = emailValidationResult.isValid;
+        var usernameIsValid = usernameValidationResult.isValid;
+        var passwordIsValid = passwordValidationResult.isValid;
+        var passwordsMatch = passwordMatchValidationResult.isValid;
+        var emailIsValid = emailValidationResult.isValid;
 
-	if (usernameIsValid && passwordIsValid && passwordsMatch && emailIsValid) {
-		registerUser(username, password, email);
-	} else if (!usernameIsValid) {
-		$('#register-error-label')
-	    		.html('Username is not valid.');
-	} else if (!passwordIsValid) {
-		$('#register-error-label')
-	    		.html('Password is not valid.');
-	} else if (!passwordsMatch) {
-		$('#register-error-label')
-	    		.html('Passwords don\'t match.');
-	} else {
-		$('#register-error-label')
-	    		.html('The email is not valid.');
-	}
+        if (usernameIsValid && passwordIsValid && passwordsMatch && emailIsValid) {
+            registerUser(username, password, email);
+        } else if (!usernameIsValid) {
+            $('#register-error-label')
+                .html('Username is not valid.');
+        } else if (!passwordIsValid) {
+            $('#register-error-label')
+                .html('Password is not valid.');
+        } else if (!passwordsMatch) {
+            $('#register-error-label')
+                .html('Passwords don\'t match.');
+        } else {
+            $('#register-error-label')
+                .html('The email is not valid.');
+        }
+    });
 });
