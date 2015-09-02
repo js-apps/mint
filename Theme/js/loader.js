64c4c611530e88f797028e993682965e919e4a14
shunobaka <vesselin_minev@abv.bg>
2015-09-02 19:52:58 +0300

    Added view controller and loader

---
+++ Theme/js/loader.js
@@ -0,0 +1,49 @@
+var loader = (function() {
+    var loader = {
+        getScript: function(scriptName) {
+            var promise = new Promise(function(resolve, reject) {
+                var url = 'js/' + scriptName + '.js';
+                $.getScript(url, function() {
+                    resolve();
+                })
+            });
+
+            return promise;
+        },
+        getPartial: function(partialName) {
+            var url = './partials/' + partialName + '.html';
+            var promise = new Promise(function(resolve, reject) {
+                $.ajax({
+                    url: url,
+                    contentType: 'text/plain',
+                    method: 'GET',
+                    success: function(data) {
+                        resolve(data);
+                    }
+                });
+            });
+
+            return promise;
+        },
+        getTemplate: function(templateName, data) {
+            var url = 'templates/' + templateName + '.handlebars';
+
+            var promise = new Promise(function(resolve, reject) {
+                $.get(url, function(templateHtml) {
+                    var template = Handlebars.compile(templateHtml);
+                    var templateWithData = {
+                        template: template,
+                        data: data
+                    };
+                    resolve(templateWithData);
+                }).fail(function() {
+                    reject();
+                });
+            });
+
+            return promise;
+        }
+    }
+
+    return loader;
+}());
---
+++ Theme/js/view-controller.js
@@ -0,0 +1,29 @@
+var viewer = (function() {
+    var viewer = {
+        updateUI: function(data) {
+            var promise = new Promise(function(resolve, reject) {
+                $('#main-container').html(data);
+                $('html, body').animate({
+                    scrollTop: 0
+                }, "slow");
+
+                resolve();
+            })
+
+            return promise;
+        },
+        parseTemplate: function(templateWithData) {
+            var promise = new Promise(function(resolve, reject) {
+                var template = templateWithData.template;
+                var data = templateWithData.data;
+                var parsedTemplate = template(data);
+
+                resolve(parsedTemplate);
+            })
+
+            return promise;
+        }
+    }
+
+    return viewer;
+}());
